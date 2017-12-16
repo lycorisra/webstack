891e421470c44158c4b04b0b65bd3478d38af0f7
lycorisra <lycorisra@yeah.net>
2017-12-16 15:42:50 +0800

    2017-12-16

---
+++ samples/webpack/webpack-dev-middleware/dev-server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var webpack = require('webpack')
+var WebpackDevMiddleware = require('webpack-dev-middleware')
+var WebpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./config/webpack.dev.config')
+var compiler = webpack(config)
+
+app = express()
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+
+app.use(WebpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  stats: { colors: true }
+}))
+app.use(WebpackHotMiddleware(compiler))
+
+var router = express.Router()
+router.get('/', function (req, res, next) {
+  res.render('index', { message: 'Hey there!'});
+})
+app.use(router)
+
+app.listen(8080, function () {
+  console.log('Listening on 8080')
+})
